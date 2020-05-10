ac958aa9b460219681f4c9ef4b29af18226d53f0
ShreeshaN <nshreesha2011@gmail.com>
2020-05-10 15:52:06 -0400

    cifar simple ffn

---
+++ difflr/experiments/hpt_experiments/dsc_fnn_hpt_mnist.py
@@ -0,0 +1,120 @@
+import torch
+from difflr.models import LinearClassifierDSC
+from difflr.data import FashionMNISTDataset
+from difflr import CONFIG
+from difflr.experiments import Tuner
+import time
+
+CONFIG.DRY_RUN = False
+
+
+def main():
+    torch.manual_seed(0)
+
+    start_time = time.time()
+    config = {
+        'model_name': 'fashionmnist_simpleffn_tuned_SGD_100p_params_100p_data',
+        "num_classes": 10,
+        'in_features': 784,
+        'epochs': 100,
+        'batch_size': [32],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+
+                'layers': [100, 50, 10]
+            },
+        'early_stopping': True,
+        'patience': 10
+    }
+
+    model = LinearClassifierDSC
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=FashionMNISTDataset, cv_split=5)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ###########################################################
+
+    start_time = time.time()
+    config = {
+        'model_name': 'fashionmnist_simpleffn_tuned_SGD_10p_params',
+        "num_classes": 10,
+        'in_features': 784,
+        'epochs': 100,
+        'batch_size': [32],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+
+                'layers': [45, 20, 10]
+            },
+        'early_stopping': True,
+        'patience': 10
+    }
+
+    model = LinearClassifierDSC
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=FashionMNISTDataset, cv_split=5)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ###########################################################
+    start_time = time.time()
+    config = {
+        'model_name': 'fashionmnist_simpleffn_tuned_SGD_10p_data',
+        "num_classes": 10,
+        'in_features': 784,
+        'epochs': 100,
+        'batch_size': [32],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+
+                'layers': [100, 50, 10]
+            },
+        'early_stopping': True,
+        'patience': 10
+    }
+
+    model = LinearClassifierDSC
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=FashionMNISTDataset, cv_split=5, data_per=10)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ###########################################################
+    start_time = time.time()
+    config = {
+        'model_name': 'fashionmnist_simpleffn_tuned_SGD_10p_params_10p_data',
+        "num_classes": 10,
+        'in_features': 784,
+        'epochs': 100,
+        'batch_size': [32],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+
+                'layers': [45, 20, 10]
+            },
+        'early_stopping': True,
+        'patience': 10
+    }
+
+    model = LinearClassifierDSC
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=FashionMNISTDataset, cv_split=5, data_per=10)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+
+if __name__ == '__main__':
+    main()
--- difflr/experiments/hpt_experiments/simple_ffn_hpt_cifar.py
+++ difflr/experiments/hpt_experiments/simple_ffn_hpt_cifar.py
@@ -24,13 +24,67 @@ CONFIG.DRY_RUN = False
 def main():
     torch.manual_seed(0)
 
+    # start_time = time.time()
+    # config = {
+    #     'model_name': 'simpleffn_tuned_SGD_cifar_100p_params_100p_data',
+    #     "num_classes": 10,
+    #     'in_features': 1024,
+    #     'epochs': 100,
+    #     'batch_size': [32],
+    #     'lr': [1e-2, 1e-3],
+    #     'lr_decay': False,
+    #     "train_p": 100,
+    #     "test_p": 100,
+    #     'dnn_config':
+    #         {
+    #
+    #             'layers': [150, 60, 10]
+    #         },
+    #     'early_stopping': True,
+    #     'patience': 10
+    #
+    # }
+    #
+    # model = LinearClassifier
+    # tuner = Tuner(config=config, model=model)
+    # tuner.tune(dataset=CIFARDataset, cv_split=5)
+    # print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ##############################################################
+
     start_time = time.time()
     config = {
-        'model_name': 'simpleffn_tuned_SGD_cifar',
+        'model_name': 'simpleffn_tuned_SGD_cifar_10p_params',
         "num_classes": 10,
         'in_features': 1024,
         'epochs': 100,
-        'batch_size': [32, 64, 256],
+        'batch_size': [32],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+                'layers': [67, 24, 10]
+            },
+        'early_stopping': True,
+        'patience': 10
+    }
+
+    model = LinearClassifier
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=CIFARDataset, cv_split=5)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ##############################################################
+
+    start_time = time.time()
+    config = {
+        'model_name': 'simpleffn_tuned_SGD_cifar_10p_data',
+        "num_classes": 10,
+        'in_features': 1024,
+        'epochs': 100,
+        'batch_size': [32],
         'lr': [1e-2, 1e-3],
         'lr_decay': False,
         "train_p": 100,
@@ -47,8 +101,38 @@ def main():
 
     model = LinearClassifier
     tuner = Tuner(config=config, model=model)
-    tuner.tune(dataset=CIFARDataset, cv_split=5)
+    tuner.tune(dataset=CIFARDataset, cv_split=5, data_per=10)
     print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ##############################################################
+
+    start_time = time.time()
+    config = {
+        'model_name': 'simpleffn_tuned_SGD_cifar_10p_params_10p_data',
+        "num_classes": 10,
+        'in_features': 1024,
+        'epochs': 100,
+        'batch_size': [32],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+
+                'layers': [67, 24, 10]
+            },
+        'early_stopping': True,
+        'patience': 10
+
+    }
+
+    model = LinearClassifier
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=CIFARDataset, cv_split=5, data_per=10)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ##############################################################
 
 
 if __name__ == '__main__':

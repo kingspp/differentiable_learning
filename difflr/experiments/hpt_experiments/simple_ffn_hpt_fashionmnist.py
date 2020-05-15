afc54dd78471ac95098ea780567ae21255bf02d1
kingspp <kingspprathyush@gmail.com>
2020-05-15 13:30:57 -0400

    Updated config

--- difflr/experiments/hpt_experiments/simple_ffn_hpt_fashionmnist.py
+++ difflr/experiments/hpt_experiments/simple_ffn_hpt_fashionmnist.py
@@ -3,7 +3,10 @@ from difflr.models import LinearClassifier
 from difflr.data import FashionMNISTDataset
 from difflr import CONFIG
 from difflr.experiments import Tuner
+from difflr.utils import check_gpu
 import time
+
+check_gpu()
 
 CONFIG.DRY_RUN = False
 
@@ -13,7 +16,60 @@ def main():
 
     start_time = time.time()
     config = {
-        'model_name': 'fashionmnist_simpleffn_tuned_adam_100p_params_100p_data',
+        'model_name': 'fashion_mnist_simpleffn_tuned_adam_100p_data_100p_params',
+        "num_classes": 10,
+        'in_features': 784,
+        'epochs': 100,
+        'batch_size': [256],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+
+                'layers': [100, 50, 10]
+            },
+        'early_stopping': True,
+        'patience': 5
+    }
+
+    model = LinearClassifier
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=FashionMNISTDataset, cv_split=5, test_interval=1)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ###########################################################
+
+    start_time = time.time()
+    config = {
+        'model_name': 'fashion_mnist_simpleffn_tuned_adam_100p_data_10p_params',
+        "num_classes": 10,
+        'in_features': 784,
+        'epochs': 100,
+        'batch_size': [256],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+
+                'layers': [45, 20, 10]
+            },
+        'early_stopping': True,
+        'patience': 5
+    }
+
+    model = LinearClassifier
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=FashionMNISTDataset, cv_split=5, test_interval=1)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ###########################################################
+    start_time = time.time()
+    config = {
+        'model_name': 'fashion_mnist_simpleffn_tuned_adam_10p_data_100p_params',
         "num_classes": 10,
         'in_features': 784,
         'epochs': 100,
@@ -28,23 +84,22 @@ def main():
                 'layers': [100, 50, 10]
             },
         'early_stopping': True,
-        'patience': 10
+        'patience': 5
     }
 
     model = LinearClassifier
     tuner = Tuner(config=config, model=model)
-    tuner.tune(dataset=FashionMNISTDataset, cv_split=5)
+    tuner.tune(dataset=FashionMNISTDataset, cv_split=5, data_per=10, test_interval=1)
     print(f"Finished tuning in {time.time() - start_time}secs")
 
     ###########################################################
-
     start_time = time.time()
     config = {
-        'model_name': 'fashionmnist_simpleffn_tuned_adam_10p_params',
+        'model_name': 'fashion_mnist_simpleffn_tuned_adam_10p_data_10p_params',
         "num_classes": 10,
         'in_features': 784,
         'epochs': 100,
-        'batch_size': [32],
+        'batch_size': [256],
         'lr': [1e-2, 1e-3],
         'lr_decay': False,
         "train_p": 100,
@@ -55,64 +110,12 @@ def main():
                 'layers': [45, 20, 10]
             },
         'early_stopping': True,
-        'patience': 10
+        'patience': 5
     }
 
     model = LinearClassifier
     tuner = Tuner(config=config, model=model)
-    tuner.tune(dataset=FashionMNISTDataset, cv_split=5)
-    print(f"Finished tuning in {time.time() - start_time}secs")
-
-    ###########################################################
-    start_time = time.time()
-    config = {
-        'model_name': 'fashionmnist_simpleffn_tuned_adam_10p_data',
-        "num_classes": 10,
-        'in_features': 784,
-        'epochs': 100,
-        'batch_size': [32],
-        'lr': [1e-2, 1e-3],
-        'lr_decay': False,
-        "train_p": 100,
-        "test_p": 100,
-        'dnn_config':
-            {
-
-                'layers': [100, 50, 10]
-            },
-        'early_stopping': True,
-        'patience': 10
-    }
-
-    model = LinearClassifier
-    tuner = Tuner(config=config, model=model)
-    tuner.tune(dataset=FashionMNISTDataset, cv_split=5, data_per=10)
-    print(f"Finished tuning in {time.time() - start_time}secs")
-
-    ###########################################################
-    start_time = time.time()
-    config = {
-        'model_name': 'fashionmnist_simpleffn_tuned_adam_10p_params_10p_data',
-        "num_classes": 10,
-        'in_features': 784,
-        'epochs': 100,
-        'batch_size': [32],
-        'lr': [1e-2, 1e-3],
-        'lr_decay': False,
-        "train_p": 100,
-        "test_p": 100,
-        'dnn_config':
-            {
-
-                'layers': [45, 20, 10]
-            },
-        'early_stopping': True,
-        'patience': 10
-    }
-
-    model = LinearClassifier
-    tuner = Tuner(config=config, model=model)
-    tuner.tune(dataset=FashionMNISTDataset, cv_split=5, data_per=10)
+    tuner.tune(dataset=FashionMNISTDataset, cv_split=5, data_per=10, test_interval=1)
     print(f"Finished tuning in {time.time() - start_time}secs")
 
 

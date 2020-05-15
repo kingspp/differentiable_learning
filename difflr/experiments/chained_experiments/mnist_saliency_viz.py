1e224bfd3ba62205958c7f90dbd82683e8a57db3
kingspp <kingspprathyush@gmail.com>
2020-05-15 06:43:42 -0400

    Added new experiments

--- difflr/experiments/chained_experiments/mnist_saliency_viz.py
+++ difflr/experiments/chained_experiments/mnist_saliency_viz.py
@@ -33,14 +33,113 @@ def cleanup_hook(model: LinearClassifierDSC):
 def main():
     torch.manual_seed(0)
     configs = [
+        # {
+        #     'model_name': 'mnist_dsc_100pd_100pp_1e1_decay',
+        #     "num_classes": 10,
+        #     'in_features': 784,
+        #     'epochs': 100,
+        #     'batch_size': 32,
+        #     'lr': 1e-1,
+        #     'lr_decay': 1,
+        #     "train_p": 100,
+        #     "test_p": 100,
+        #     'dnn_config':
+        #         {
+        #
+        #             'layers': [150, 60, 10]
+        #         },
+        #     'early_stopping': True,
+        #     'patience': 5
+        # },
+        #
+        # {
+        #     'model_name': 'mnist_dsc_100pd_100pp_1e1_decay_reg1',
+        #     "num_classes": 10,
+        #     'in_features': 784,
+        #     'epochs': 100,
+        #     'batch_size': 32,
+        #     'lr': 1e-1,
+        #     'lr_decay': 1,
+        #     'reg_coeff': 1,
+        #     "train_p": 100,
+        #     "test_p": 100,
+        #     'dnn_config':
+        #         {
+        #
+        #             'layers': [150, 60, 10]
+        #         },
+        #     'early_stopping': True,
+        #     'patience': 5
+        # },
+        #
+        # {
+        #     'model_name': 'mnist_dsc_100pd_100pp_1e1_decay_reg01',
+        #     "num_classes": 10,
+        #     'in_features': 784,
+        #     'epochs': 100,
+        #     'batch_size': 32,
+        #     'lr': 1e-1,
+        #     'lr_decay': 1,
+        #     'reg_coeff': 0.1,
+        #     "train_p": 100,
+        #     "test_p": 100,
+        #     'dnn_config':
+        #         {
+        #
+        #             'layers': [150, 60, 10]
+        #         },
+        #     'early_stopping': True,
+        #     'patience': 5
+        # },
+        #
+        # {
+        #     'model_name': 'mnist_dsc_100pd_100pp_1e1_decay_reg001',
+        #     "num_classes": 10,
+        #     'in_features': 784,
+        #     'epochs': 100,
+        #     'batch_size': 32,
+        #     'lr': 1e-1,
+        #     'lr_decay': 1,
+        #     'reg_coeff': 0.01,
+        #     "train_p": 100,
+        #     "test_p": 100,
+        #     'dnn_config':
+        #         {
+        #
+        #             'layers': [150, 60, 10]
+        #         },
+        #     'early_stopping': True,
+        #     'patience': 5
+        # },
+        #
+        # {
+        #     'model_name': 'mnist_dsc_100pd_100pp_1e2',
+        #     "num_classes": 10,
+        #     'in_features': 784,
+        #     'epochs': 100,
+        #     'batch_size': 32,
+        #     'lr': 1e-2,
+        #     # 'lr_decay': 1,
+        #     "train_p": 100,
+        #     "test_p": 100,
+        #     'dnn_config':
+        #         {
+        #
+        #             'layers': [150, 60, 10]
+        #         },
+        #     'early_stopping': True,
+        #     'patience': 5
+        # },
+        #
         {
-            'model_name': 'mnist_dsc_100pd_100pp_1e1_decay',
+            'model_name': 'mnist_dsc_100pd_100pp_1e2_reg1',
             "num_classes": 10,
             'in_features': 784,
             'epochs': 100,
             'batch_size': 32,
-            'lr': 1e-1,
-            'lr_decay': 1,
+            'lr': 1e-2,
+            # 'lr_decay': 1,
+            'reg_coeff': 1,
             "train_p": 100,
             "test_p": 100,
             'dnn_config':
@@ -53,33 +152,13 @@ def main():
         },
 
         {
-            'model_name': 'mnist_dsc_100pd_100pp_1e1_decay_reg1',
+            'model_name': 'mnist_dsc_100pd_100pp_1e2_reg01',
             "num_classes": 10,
             'in_features': 784,
             'epochs': 100,
             'batch_size': 32,
-            'lr': 1e-1,
-            'lr_decay': 1,
-            'reg_coeff':1,
-            "train_p": 100,
-            "test_p": 100,
-            'dnn_config':
-                {
-
-                    'layers': [150, 60, 10]
-                },
-            'early_stopping': True,
-            'patience': 5
-        },
-
-        {
-            'model_name': 'mnist_dsc_100pd_100pp_1e1_decay_reg01',
-            "num_classes": 10,
-            'in_features': 784,
-            'epochs': 100,
-            'batch_size': 32,
-            'lr': 1e-1,
-            'lr_decay': 1,
+            'lr': 1e-2,
+            # 'lr_decay': 1,
             'reg_coeff': 0.1,
             "train_p": 100,
             "test_p": 100,
@@ -91,15 +170,14 @@ def main():
             'early_stopping': True,
             'patience': 5
         },
-
         {
-            'model_name': 'mnist_dsc_100pd_100pp_1e1_decay_reg001',
+            'model_name': 'mnist_dsc_100pd_100pp_1e2_reg001',
             "num_classes": 10,
             'in_features': 784,
             'epochs': 100,
             'batch_size': 32,
-            'lr': 1e-1,
-            'lr_decay': 1,
+            'lr': 1e-2,
+            # 'lr_decay': 1,
             'reg_coeff': 0.01,
             "train_p": 100,
             "test_p": 100,
@@ -111,15 +189,15 @@ def main():
             'early_stopping': True,
             'patience': 5
         },
-
         {
-            'model_name': 'mnist_dsc_100pd_100pp_1e2',
+            'model_name': 'mnist_dsc_100pd_100pp_1e2_reg0001',
             "num_classes": 10,
             'in_features': 784,
             'epochs': 100,
             'batch_size': 32,
             'lr': 1e-2,
             # 'lr_decay': 1,
+            'reg_coeff': 0.001,
             "train_p": 100,
             "test_p": 100,
             'dnn_config':
@@ -136,8 +214,7 @@ def main():
         model = LinearClassifierDSC(config=config)
         epoch_end_hook(model)
         model.fit(dataset=MNISTDataset, epoch_end_hook=epoch_end_hook, cleanup_hook=cleanup_hook, test_interval=-1)
-        print(f'PERCENTAGE COMPLETED: {(e+1) / len(configs):.2f}')
-
+        print(f'PERCENTAGE COMPLETED: {(e + 1) / len(configs):.2f}')
 
 
 if __name__ == '__main__':

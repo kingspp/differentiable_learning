76d9f3b36c7da1ee95f6ef57b768b1dacd7e452b
kingspp <kingspprathyush@gmail.com>
2020-05-15 15:28:08 -0400

    Updated experiments

--- difflr/experiments/hpt_experiments/simple_ffn_hpt_cifar.py
+++ difflr/experiments/hpt_experiments/simple_ffn_hpt_cifar.py
@@ -16,20 +16,73 @@ from difflr.models import LinearClassifier
 from difflr.data import CIFARDataset
 from difflr import CONFIG
 from difflr.experiments import Tuner
+from difflr.utils import check_gpu
 import time
 
 CONFIG.DRY_RUN = False
 
-if not torch.cuda.is_available():
-    print('GPU not available')
-    exit()
+check_gpu()
 
 def main():
     torch.manual_seed(0)
 
     start_time = time.time()
     config = {
-        'model_name': 'cifar_simpleffn_tuned_SGD_100p_params_100p_data',
+        'model_name': 'cifar_simpleffn_tuned_adam_100p_data_100p_params',
+        "num_classes": 10,
+        'in_features': 1024,
+        'epochs': 100,
+        'batch_size': [256],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+
+                'layers': [150, 60, 10]
+            },
+        'early_stopping': True,
+        'patience': 10
+
+    }
+
+    model = LinearClassifier
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=CIFARDataset, cv_split=5, test_interval=1)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ##############################################################
+
+    start_time = time.time()
+    config = {
+        'model_name': 'cifar_simpleffn_tuned_adam_100p_data_10p_params',
+        "num_classes": 10,
+        'in_features': 1024,
+        'epochs': 100,
+        'batch_size': [256],
+        'lr': [1e-2, 1e-3],
+        'lr_decay': False,
+        "train_p": 100,
+        "test_p": 100,
+        'dnn_config':
+            {
+                'layers': [35, 20, 10]
+            },
+        'early_stopping': True,
+        'patience': 10
+    }
+
+    model = LinearClassifier
+    tuner = Tuner(config=config, model=model)
+    tuner.tune(dataset=CIFARDataset, cv_split=5, test_interval=1)
+    print(f"Finished tuning in {time.time() - start_time}secs")
+
+    ##############################################################
+
+    start_time = time.time()
+    config = {
+        'model_name': 'cifar_simpleffn_tuned_adam_10p_data_100p_params',
         "num_classes": 10,
         'in_features': 1024,
         'epochs': 100,
@@ -50,68 +103,14 @@ def main():
 
     model = LinearClassifier
     tuner = Tuner(config=config, model=model)
-    tuner.tune(dataset=CIFARDataset, cv_split=5)
+    tuner.tune(dataset=CIFARDataset, cv_split=5, data_per=10, test_interval=1)
     print(f"Finished tuning in {time.time() - start_time}secs")
 
     ##############################################################
 
     start_time = time.time()
     config = {
-        'model_name': 'cifar_simpleffn_tuned_SGD_10p_params',
-        "num_classes": 10,
-        'in_features': 1024,
-        'epochs': 100,
-        'batch_size': [32],
-        'lr': [1e-2, 1e-3],
-        'lr_decay': False,
-        "train_p": 100,
-        "test_p": 100,
-        'dnn_config':
-            {
-                'layers': [35, 20, 10]
-            },
-        'early_stopping': True,
-        'patience': 10
-    }
-
-    model = LinearClassifier
-    tuner = Tuner(config=config, model=model)
-    tuner.tune(dataset=CIFARDataset, cv_split=5)
-    print(f"Finished tuning in {time.time() - start_time}secs")
-
-    ##############################################################
-
-    start_time = time.time()
-    config = {
-        'model_name': 'cifar_simpleffn_tuned_SGD_10p_data',
-        "num_classes": 10,
-        'in_features': 1024,
-        'epochs': 100,
-        'batch_size': [32],
-        'lr': [1e-2, 1e-3],
-        'lr_decay': False,
-        "train_p": 100,
-        "test_p": 100,
-        'dnn_config':
-            {
-
-                'layers': [150, 60, 10]
-            },
-        'early_stopping': True,
-        'patience': 10
-
-    }
-
-    model = LinearClassifier
-    tuner = Tuner(config=config, model=model)
-    tuner.tune(dataset=CIFARDataset, cv_split=5, data_per=10)
-    print(f"Finished tuning in {time.time() - start_time}secs")
-
-    ##############################################################
-
-    start_time = time.time()
-    config = {
-        'model_name': 'cifar_simpleffn_tuned_SGD_10p_params_10p_data',
+        'model_name': 'cifar_simpleffn_tuned_adam_10p_data_10p_params',
         "num_classes": 10,
         'in_features': 1024,
         'epochs': 100,
@@ -132,7 +131,7 @@ def main():
 
     model = LinearClassifier
     tuner = Tuner(config=config, model=model)
-    tuner.tune(dataset=CIFARDataset, cv_split=5, data_per=10)
+    tuner.tune(dataset=CIFARDataset, cv_split=5, data_per=10, test_interval=1)
     print(f"Finished tuning in {time.time() - start_time}secs")
 
     ##############################################################
